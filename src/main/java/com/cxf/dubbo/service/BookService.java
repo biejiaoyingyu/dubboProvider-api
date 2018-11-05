ab87c1ab5def46d8f16d88200716082d43720991
chenxiaofang <1024643903@qq.com>
2018-11-05 17:42:39 +0800

    mod

--- src/main/java/com/cxf/dubbo/service/BookService.java
+++ src/main/java/com/cxf/dubbo/service/BookService.java
@@ -5,9 +5,8 @@ import com.cxf.dubbo.entities.Book;
 import java.util.List;
 
 public interface BookService {
+  Book getOneBook4Callback(Integer id);
   Book getOneBook(int i);
   List<Book> qryAllBook();
-
-
 
 }
